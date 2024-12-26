3732a47a8b0acc778ee364c9854ec14800d96609
destiny739 <gregorydestiny103@gmail.com>
2024-12-27 00:48:47 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7931290340:AAGDZprZYKbHmlOdvW6t4SGpcLheA-G5cUQ", // Token bot Telegram
+  OWNER_ID: "6804268009", // ID pemilik bot
+  allowedGroupIds: [-6857250857, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
